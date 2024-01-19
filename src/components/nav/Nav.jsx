a67d5ecfa45e7cc9fbb982ed923052851a1b0727
Vytautas <v.stankeviciuss@gmail.com>
2024-01-19 13:21:09 +0200

    first commit

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,18 @@
+import { Button,ButtonGroup } from "react-bootstrap"
+import {filterTodo} from "../../store/actions/TodoActions"
+import { connect } from "react-redux"
+
+const Nav = (props)=>{
+
+const {filterTodo} = props;
+    
+    return(
+        <ButtonGroup style={{marginLeft:'156px'}}>
+            <Button color='primary' onClick={()=>{filterTodo('VISOS')}}>Visos uzduotys</Button>
+            <Button color='danger' onClick={()=>{filterTodo('AKTYVIOS')}}>Aktyvios</Button>
+            <Button color='success' onClick={()=>{filterTodo('PABAIGTOS')}}>Uzbaigtos</Button>
+        </ButtonGroup>
+    )
+}
+
+export default connect(null,{filterTodo})(Nav)
---
+++ src/components/todo/TodoList.jsx
@@ -0,0 +1,33 @@
+import TodoItem from "./TodoItem"
+import { connect } from "react-redux"
+import {markDone, deleteTodo} from '../../store/actions/TodoActions'
+import filterTodos from "../../utilities/filterTodos"
+import { ListGroup } from "react-bootstrap"
+
+
+const TodoList = (props)=>{
+    let {todos,markDone,status,deleteTodo} = props;
+    if(todos.length === 0) return <p>Nera ivestu uzduociu</p>
+    let filterTodoList = filterTodos(todos,status)
+    console.log(props)
+    return(
+        <div className="container">
+            <ListGroup>
+                {filterTodoList.map(todo=>
+                    <TodoItem
+                    todo={todo}
+                    key={todo.id}
+                    mark={markDone}
+                    remove={deleteTodo}/>
+                    )}
+            </ListGroup>
+        </div>
+    )
+}
+
+const mapDispatchToProps = state => ({
+    todos:state.todos.todos,
+    status:state.todos.filter
+})
+
+export default connect(mapDispatchToProps,{markDone, deleteTodo})(TodoList)
